9619da3f167b984fa2d3a826c2789d75b3f92008
AbiPol <35527096+AbiPol@users.noreply.github.com>
2018-02-01 21:39:21 +0000

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', // Este componente se rendiza en la etiqueta que indica
   templateUrl: './app.component.html', //ruta relativa donde esta la plantilla HTML
-  styleUrls: ['./app.component.css'] //Es un array donde se da la ruta relativa de losarchivos .CSS
+  styleUrls: ['./app.component.css'] //Es un array donde se da la ruta relativa de losarchivos .CSS 
 })
 
 //Se exporta la clase para poder utilizar nuestra clase. Aqui va la logica de nuestro
